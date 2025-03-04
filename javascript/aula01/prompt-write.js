b959fcacf781b58b2473dd354e283e5b3185cb96
Eike Frota <eikefrota@gmail.com>
2025-03-04 15:57:08 -0300

    Removendo repositórios internos e transformando em pastas normais

---
+++ javascript/aula01/prompt-write.js
@@ -0,0 +1,8 @@
+var nome = prompt("Digite seu nome: ")
+var sobrenome = prompt("Digite seu sobrenome: ")
+
+console.log("Olá " + " " + sobrenome)
+
+document.write("<h1> Olá " + nome + "</h1>")
+
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto Steve Jobs' ")
---
+++ javascript/aula02/desafio.js
@@ -0,0 +1,8 @@
+// Crie uma string que contenha o dia de hoje, exemplo: 
+
+// let hoje = '04/03/2025';
+
+//  Separe essa data em variaveis dia onde contenha apenas o dia, mes onde contenha apenas o mes, e ano onde contenha apenas o ano.
+// Obs: Esse desafio não precisa ser entregue, mas tente fazer sem olhar no resuldo :)
+
+let hoje = '04/03/2025';
