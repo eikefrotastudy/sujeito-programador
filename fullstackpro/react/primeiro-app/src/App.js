62b5ce766f2b953975f7862c5d3e73c80382afd7
Eike Frota <eikefrota@gmail.com>
2025-03-04 22:03:59 -0300

    Iniciando primeiros passos no ReactJS

---
+++ fullstackpro/react/primeiro-app/src/App.js
@@ -0,0 +1,14 @@
+import Nome from "./components/Nome";
+
+function App() {
+  return (
+    <div>
+      <h1>Bem-vindo ao meu projeto!</h1>
+      <Nome aluno="Eike" idade="20"/>
+      <br/>
+      <Nome aluno="Enzo" idade="23"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ fullstackpro/react/primeiro-app/src/components/Nome.js
@@ -0,0 +1,7 @@
+function Nome({aluno, idade}){ // props
+    return(
+        <span>Bem-vindo: {aluno} - Idade: {idade}</span> // props.aluno
+    )
+}
+
+export default Nome
