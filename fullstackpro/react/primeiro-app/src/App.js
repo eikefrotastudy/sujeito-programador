fd71f05b6ed9de187548e09e1fccccfe9c1a2f87
Eike Frota <eikefrota@gmail.com>
2025-03-06 16:24:38 -0300

    Manipulando Formulários

--- fullstackpro/react/primeiro-app/src/App.js
+++ fullstackpro/react/primeiro-app/src/App.js
@@ -1,19 +1,53 @@
 import { useState } from "react";
 
 function App() {
-  const [aluno, setAluno] = useState("Eike");
+  const [nome, setName] = useState("");
+  const [idade, setIdade] = useState("");
+  const [email, setEmail] = useState("");
 
-  function mudarNome(nome) {
-    setAluno(nome);
+  const [user, setUser] = useState({});
+
+  function handleRegister(event) {
+    event.preventDefault();
+
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email
+    })
+
   }
 
   return (
     <div>
-      <h1>Bem-vindo ao meu projeto!</h1>
-      <h2>Aluno: {aluno}</h2>
-      <button onClick={ () => mudarNome("Eike Frota") }>
-        Alterar Nome
-      </button>
+      <h1>Cadastro</h1>
+      <form onSubmit={handleRegister}>
+        <label>Nome</label><br/>
+        <input placeholder="Digite seu nome:" 
+        value={nome}
+        onChange={event => setName(event.target.value)}/><br/>
+        
+        <label>Idade</label><br/>
+        <input placeholder="Digite sua idade:" 
+        value={idade}
+        onChange={event => setIdade(event.target.value)}/><br/>
+        
+        <label>Email</label><br/>
+        <input placeholder="Digite seu email:" 
+        value={email}
+        onChange={event => setEmail(event.target.value)}/><br/>
+        
+        <button type="submit">Enviar</button>
+      </form>
+      <br/>
+      <br/>
+
+      <div>
+        <span>Nome: {user.nome}</span><br/>
+        <span>Idade: {user.idade} </span><br/>
+        <span>Email: {user.email} </span><br/>
+      </div>
+
     </div>
   );
 }
