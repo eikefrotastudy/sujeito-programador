ddfb358179d9cc371d605b1889478b82ea994d96
Eike Frota <eikefrotaa@hotmail.com>
2025-03-08 17:24:01 -0300

    Trabalhando com arrays no ReactJS

--- fullstackpro/react/primeiro-app/src/App.js
+++ fullstackpro/react/primeiro-app/src/App.js
@@ -1,52 +1,38 @@
 import { useState } from "react";
 
 function App() {
-  const [nome, setName] = useState("");
-  const [idade, setIdade] = useState("");
-  const [email, setEmail] = useState("");
-
-  const [user, setUser] = useState({});
+  const [input, setInput] = useState('')
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar ReactJS'
+  ]);
 
   function handleRegister(event) {
     event.preventDefault();
 
-    setUser({
-      nome: nome,
-      idade: idade,
-      email: email
-    })
-
+    setTarefas([...tarefas, input]);
+    setInput('');
   }
 
   return (
     <div>
-      <h1>Cadastro</h1>
+      <h1>Cadastro de Tarefas</h1>
       <form onSubmit={handleRegister}>
-        <label>Nome</label><br/>
-        <input placeholder="Digite seu nome:" 
-        value={nome}
-        onChange={event => setName(event.target.value)}/><br/>
-        
-        <label>Idade</label><br/>
-        <input placeholder="Digite sua idade:" 
-        value={idade}
-        onChange={event => setIdade(event.target.value)}/><br/>
-        
-        <label>Email</label><br/>
-        <input placeholder="Digite seu email:" 
-        value={email}
-        onChange={event => setEmail(event.target.value)}/><br/>
-        
+        <label>Nome da tarefa</label><br/>
+        <input placeholder="Digite sua tarefa:" 
+        value={input}
+        onChange={event => setName(event.target.value)}
+        /><br/>
         <button type="submit">Enviar</button>
       </form>
       <br/>
       <br/>
 
-      <div>
-        <span>Nome: {user.nome}</span><br/>
-        <span>Idade: {user.idade} </span><br/>
-        <span>Email: {user.email} </span><br/>
-      </div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
 
     </div>
   );
